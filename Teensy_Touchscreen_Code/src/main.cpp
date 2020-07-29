53c64eaae81d24121f03c504851f28c99cfeeee4
MxFxM <32713448+MxFxM@users.noreply.github.com>
2020-07-29 20:16:19 +0200

    Mapping of output using calibration values

--- Teensy_Touchscreen_Code/src/main.cpp
+++ Teensy_Touchscreen_Code/src/main.cpp
@@ -1,10 +1,22 @@
 #include <Arduino.h>
 
 // Connections to the touchscreen
-#define X_AXIS_RIGHT 14
-#define X_AXIS_LEFT 16
-#define Y_AXIS_TOP 15
-#define Y_AXIS_BOTTOM 17
+#define X_AXIS_RIGHT 15
+#define X_AXIS_LEFT 17
+#define Y_AXIS_TOP 16
+#define Y_AXIS_BOTTOM 14
+
+// Calibration values
+#define CAL_BOTTOM 860
+#define CAL_TOP 100
+#define CAL_LEFT 90
+#define CAL_RIGHT 950
+
+// Mapping values
+#define MAP_LEFT 0
+#define MAP_RIGHT 100
+#define MAP_TOP 0
+#define MAP_BOTTOM 100
 
 void setup() {
   Serial.begin(9600); // Usb is always 12 Mbit/s
@@ -31,6 +43,22 @@ void loop() {
   digitalWriteFast(Y_AXIS_TOP, LOW);
   delay(10);
   int y_axis = analogRead(X_AXIS_LEFT);
+
+  x_axis = map(x_axis, CAL_LEFT, CAL_RIGHT,  MAP_LEFT, MAP_RIGHT);
+  y_axis = map(y_axis, CAL_TOP, CAL_BOTTOM,  MAP_TOP, MAP_BOTTOM);
+
+  if (x_axis > MAP_RIGHT) {
+    x_axis = MAP_RIGHT;
+  }
+  if (x_axis < MAP_LEFT) {
+    x_axis = MAP_LEFT;
+  }
+  if (y_axis > MAP_BOTTOM) {
+    y_axis = MAP_BOTTOM;
+  }
+  if (y_axis < MAP_TOP) {
+    y_axis = MAP_TOP;
+  }
 
   Serial.print("X: ");
   Serial.print(x_axis);
